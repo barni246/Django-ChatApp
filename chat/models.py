7c0738d2dc4a62f73d3a6e347c32aadf70c18319
Barnabas Gonda <barnabas.gonda@googlemail.com>
2023-12-17 17:45:32 +0100

    requirements.txt

--- chat/models.py
+++ chat/models.py
@@ -9,6 +9,7 @@ class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default = date.today)
     #  chat
+    # barni
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
     
