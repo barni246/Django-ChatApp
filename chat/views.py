9460e435c70d1fafd77213ff8e6ca19dd2b8f2a3
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-01-05 13:26:36 +0100

    show chat messages

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-
-# def index(request):
-   # if request.method == 'POST' and input != '':
-   #  print("Recived data: " + request.POST['textmessage'])
-   # return render(request, 'chat/index.html', {'username': 'Barni'})
+from .models import Message, Chat
 
 def index(request):
-    if request.method == 'POST' and request.POST.get('textmessage', '') != '':
-        received_data = request.POST['textmessage']
-        print("Received data: " + received_data)
-    return render(request, 'chat/index.html', {'username': 'Barni'})
+   myChat = Chat.objects.get(id=1)
+   if request.method == 'POST':
+        print("Recived data: " + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+   chatMessages = Message.objects.filter(chat__id=1)
+   return render(request, 'chat/index.html', {'messages': chatMessages})
+
+# def index(request):
+#     if request.method == 'POST' and request.POST.get('textmessage', '') != '':
+#         received_data = request.POST['textmessage']
+#         print("Received data: " + received_data)
+#     return render(request, 'chat/index.html', {'username': 'Barni'})
+
+
+def login_view(request):
+    return render(request, 'auth/login.html')
