cd1c728de886d4b2a697cdb02ae0bbbf5d433e9f
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-01-07 21:08:30 +0100

    wrong_form register.html

--- chat/views.py
+++ chat/views.py
@@ -30,11 +30,34 @@ def login_view(request):
     return render(request, 'auth/login.html', {'redirect': redirect})
 
 
+# def register_view(request):
+#     if request.method == 'POST':
+#         new_username = request.POST.get('username')
+#         new_email = request.POST.get('email')
+#         new_password = request.POST.get('password')
+#         confirm_password = request.POST.get('password')
+#         user = User.objects.create_user(username = new_username, email = new_email , password = new_password)
+#         if new_password == confirm_password:
+#             return HttpResponseRedirect('/login/')
+#         else:
+#             return render(request, 'auth/register.html')
+#     return render(request, 'auth/register.html')
+
+
 def register_view(request):
     if request.method == 'POST':
-        user = User.objects.create_user(username = request.POST.get('username'), email = request.POST.get('email') , password = request.POST.get('password'))
-        if user:
-            return HttpResponseRedirect('/login/')
-        else:
-            return render(request, 'auth/register.html', {'invalidRegister': True})
+        new_username = request.POST.get('username')
+        new_email = request.POST.get('email')
+        new_password = request.POST.get('password')
+        confirm_password = request.POST.get('confirmPassword')
+        if new_password == confirm_password:
+            user = User.objects.create_user(username=new_username, email=new_email, password=new_password)
+            if user:
+                return HttpResponseRedirect('/login/',{'wrong_form':False})
+        return render(request, 'auth/register.html', {'wrong_form':True})
     return render(request, 'auth/register.html')
+
+
+
+
+
