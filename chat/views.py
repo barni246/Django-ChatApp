e898743f3113131569adb31fa14b607940c0b1cb
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-01-14 12:34:42 +0100

    json format

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Message, Chat
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
 
 
 @login_required(login_url = '/login/')
@@ -12,7 +13,9 @@ def index(request):
    if request.method == 'POST' and request.POST.get('textmessage', '') != '':
         print("Recived data: " + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        new_message =  Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        serialized_obj = serializers.serialize('json', [new_message,])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
    chatMessages = Message.objects.filter(chat__id=1)
    return render(request, 'chat/index.html', {'messages': chatMessages})
 
